ca1a16d7b057c0aab8e1ea843532a27a1e9b1e2f
Alexandros Schillings <aschillings@gmail.com>
2017-09-27 13:55:06 +0100

    We now have a main logging class, and keep track of errors better.

---
+++ src/main/java/uk/co/alt236/gradlecache2mvn/util/Logger.java
@@ -0,0 +1,47 @@
+package uk.co.alt236.gradlecache2mvn.util;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public final class Logger {
+
+    private static AtomicReference<Mode> currentMode = new AtomicReference<>(Mode.IMPORTANT);
+
+    public static void log(final String template, Object... params) {
+        log(String.format(template, params));
+    }
+
+    public static void log(final String message) {
+        if (currentMode.get() == Mode.ALL) {
+            out(message);
+        }
+    }
+
+    public static void logImportant(final String template, Object... params) {
+        logImportant(String.format(template, params));
+    }
+
+    public static void logImportant(final String message) {
+        out(message);
+    }
+
+    public static void logError(final String message) {
+        err("ERROR: " + message);
+    }
+
+    private static void setMode(final Mode mode) {
+        currentMode.set(mode);
+    }
+
+    private static void out(final String message) {
+        System.out.println(message);
+    }
+
+    private static void err(final String message) {
+        System.err.println(message);
+    }
+
+    public enum Mode {
+        ALL,
+        IMPORTANT
+    }
+}
